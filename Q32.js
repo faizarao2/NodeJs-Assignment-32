b0655361c516b43ce5196e295c2f90fa871ff90e
Faiza Rao <159902088+faizarao2@users.noreply.github.com>
2024-02-23 19:51:52 +0500

    Add files via upload

---
+++ Q32.js
@@ -0,0 +1,25 @@
+// 32. Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+// • Make a list of five or more usernames called current_users.
+// • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+// • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+// List of current users
+var currentUsers = ['john', 'smith', 'alex', 'emma', 'chris'];
+// List of new users
+var newUsers = ['maria', 'Alex', 'peter', 'miller', 'Smith'];
+// Function to check if a username is available
+function isUsernameAvailable(username) {
+    // Convert both username arrays to lowercase for case-insensitive comparison
+    var currentUsersLowercase = currentUsers.map(function (user) { return user.toLowerCase(); });
+    var usernameLowercase = username.toLowerCase();
+    return !currentUsersLowercase.includes(usernameLowercase);
+}
+// Loop through new_users list and check availability of each username
+newUsers.forEach(function (newUser) {
+    if (isUsernameAvailable(newUser)) {
+        console.log("The username '".concat(newUser, "' is available."));
+    }
+    else {
+        console.log("Sorry, the username '".concat(newUser, "' is already taken. Please choose another one."));
+    }
+});
